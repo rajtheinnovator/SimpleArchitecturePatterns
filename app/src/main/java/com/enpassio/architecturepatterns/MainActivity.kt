91dd453a857dd1cb0c4b5f7b06f10da778e80036
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2018-10-07 15:47:01 +0530

    Code refactoring to handle merge conflict

--- app/src/main/java/com/enpassio/architecturepatterns/MainActivity.kt
+++ app/src/main/java/com/enpassio/architecturepatterns/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import com.enpassio.architecturepatterns.mvp.MvpPatternActivity
+import com.enpassio.architecturepatterns.mvpExample.MvpPatternActivity
 import com.enpassio.architecturepatterns.mvpwithoutrxanddagger.view.MvpWithoutRxAndDaggerActivity
 
 class MainActivity : AppCompatActivity() {
